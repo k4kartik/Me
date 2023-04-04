eb41a70710e996a5d36d302a9a77f99eb374359a
kartik gupta <guptakartik129@gmail.com>
2023-04-05 02:48:57 +0530

    website
    Files for website

---
+++ script.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["UI/UX Designer", "Developer", "Tech Blogger", "Shell Scripter"],
+        typeSpeed: 100,
+        backSpeed: 70,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["UI/UX Designer", "Developer", "Tech Blogger", "Shell Scripter"],
+        typeSpeed: 100,
+        backSpeed: 70,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+window.addEventListener("scroll", function() {
+    const skillsSection = document.getElementById("skills");
+    const skillsSectionTop = skillsSection.offsetTop;
+    const skillsSectionHeight = skillsSection.offsetHeight;
+    const windowHeight = window.innerHeight;
+    const windowScroll = window.scrollY;
+
+    // If the user has scrolled to the skills section, add the "animate" class to the percentage bars
+    if (windowScroll > skillsSectionTop + skillsSectionHeight - windowHeight) {
+        const percentageBars = document.querySelectorAll(".skills .skills-content .right .line");
+        percentageBars.forEach(function(bar) {
+            const percentage = bar.previousElementSibling.lastElementChild.innerHTML;
+            bar.style.setProperty("--percentage", percentage);
+            bar.classList.add("animate");
+        });
+    }
+});
